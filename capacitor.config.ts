c4deda3fd7665954a386dca4f795cc40040b8a0a
Yhyaxxx5 <yhyaxxx5@gmail.com>
2026-06-21 15:08:02 +0000

    Add initial Capacitor configuration file

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.creativehub.app',
+  appName: 'Creative Hub',
+  webDir: '.',
+  bundledWebRuntime: false
+};
+
+export default config;
